ab06fb70fa2c35cb1270b6bdcd88a1a7a15af0ab
BlackHole00 <lastapocalypse9@gmail.com>
2025-05-07 11:17:01 +0200

    Runner: Forgotten Changes

--- src/runner/algorithm_runner/algorithm_runner.c
+++ src/runner/algorithm_runner/algorithm_runner.c
@@ -281,9 +281,9 @@ static Algorithm_Runner_Result algorithmrunner_test_algorithm_with_array(
 	struct timespec start_time;
 	struct timespec end_time;
 
-	clock_gettime(CLOCK_MONOTONIC_RAW, &start_time);
+	clock_gettime(CLOCK_MONOTONIC, &start_time);
 	algorithm->algorithm_callback(array, element_count, &test_stats->stats);
-	clock_gettime(CLOCK_MONOTONIC_RAW, &end_time);
+	clock_gettime(CLOCK_MONOTONIC, &end_time);
 
 	if (!check_if_sorted(array, element_count)) {
 		return ALGORITHM_RUNNER_RESULT_SORTING_ERROR;

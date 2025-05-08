aa54283db1d729cb3bf60d99d62b943457ca850d
BlackHole00 <lastapocalypse9@gmail.com>
2025-05-08 19:53:20 +0200

    Exercises: AVLTree

---
+++ src/exercises/22_avl_tree.c
@@ -0,0 +1,472 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+
+////////////////////////////////////////////////////////////////////////////////
+typedef struct AVL_Node {
+	int key;
+	const char* value;
+
+	struct AVL_Node* parent;
+	struct AVL_Node* left;
+	struct AVL_Node* right;
+
+	// Dato da height(right) - height(left)
+	// Valori OK: -1, 0, 1. Altri valori richiedono bilanciamento
+	int balance_factor;
+} AVL_Node;
+
+AVL_Node* avlnode_new(AVL_Node* parent, int key, const char* value) {
+	AVL_Node* node = malloc(sizeof(AVL_Node));
+	assert(node != NULL);
+
+	node->key = key;
+	node->value = strdup(value);
+
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+
+	node->balance_factor = 0;
+
+	return node;
+}
+
+void avlnode_free(AVL_Node* node) {
+	assert(node != NULL);
+
+	free((void*)node->value);
+	free(node);
+}
+
+AVL_Node* avlnode_successor(AVL_Node* node) {
+	assert(node != NULL);
+
+	AVL_Node* current_node = node->right;
+	if (current_node == NULL) {
+		return NULL;
+	}
+
+	while (current_node->left != NULL) {
+		current_node = node->left;
+	}
+
+	return current_node;
+}
+
+int avlnode_height(AVL_Node* node) {
+	if (node == NULL) {
+		return 0;
+	}
+
+	int left_height = avlnode_height(node->left);
+	int right_height = avlnode_height(node->right);
+
+	if (left_height > right_height) {
+		return left_height + 1;
+	} else {
+		return right_height + 1;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+typedef struct {
+	AVL_Node* root;
+} AVL_Tree;
+
+typedef enum {
+	AVL_TREE_REBALANCE_RIGHT,
+	AVL_TREE_REBALANCE_LEFT,
+	AVL_TREE_REBALANCE_LEFT_RIGHT,
+	AVL_TREE_REBALANCE_RIGHT_LEFT,
+} AVL_Tree_Rebalance_Action;
+
+typedef enum {
+	AVL_TREE_BALANCE_NEW_NODE,
+	AVL_TREE_BALANCE_DELETED_NODE,
+} AVL_Tree_Balance_Change;
+
+void avltree_create(AVL_Tree* tree) {
+	tree->root = NULL;
+}
+
+void avltree_destroy_helper(AVL_Node* node) {
+	if (node == NULL) {
+		return;
+	}
+
+	avltree_destroy_helper(node->left);
+	avltree_destroy_helper(node->right);
+
+	avlnode_free(node);
+}
+
+void avltree_destroy(AVL_Tree tree) {
+	avltree_destroy_helper(tree.root);
+}
+
+void avltree_rotate_left(AVL_Tree* tree, AVL_Node* node) {
+	assert(tree != NULL);
+	assert(node != NULL);
+
+	AVL_Node* right = node->right;
+	if (right == NULL) {
+		return;
+	}
+
+	node->right = right->left;
+	if (right->left != NULL) {
+		right->left->parent = node;
+	}
+
+	if (node->parent == NULL) {
+		tree->root = right;
+		right->parent = NULL;
+	} else {
+		right->parent = node->parent;
+		if (node->parent->left == node) {
+			node->parent->left = right;
+		} else {
+			node->parent->right = right;
+		}
+	}
+
+	right->left = node;
+	node->parent = right;
+
+	node->balance_factor -= 2;
+	right->balance_factor -= 1;
+}
+
+void avltree_rotate_right(AVL_Tree* tree, AVL_Node* node) {
+	assert(tree != NULL);
+	assert(node != NULL);
+
+	AVL_Node* left = node->left;
+	if (left == NULL) {
+		return;
+	}
+
+	node->left = left->right;
+	if (left->right != NULL) {
+		left->right->parent = node;
+	}
+
+	if (node->parent == NULL) {
+		tree->root = left;
+		left->parent = NULL;
+	} else {
+		left->parent = node->parent;
+		if (node->parent->left == node) {
+			node->parent->left = left;
+		} else {
+			node->parent->right = left;
+		}
+	}
+
+	left->right = node;
+	node->parent = left;
+
+	node->balance_factor += 2;
+	left->balance_factor += 1;
+
+}
+
+void avltree_rotate_left_right(AVL_Tree* tree, AVL_Node* node) {
+	assert(tree != NULL);
+	assert(node != NULL);
+
+	AVL_Node* left = node->left;
+	if (left == NULL) {
+		return;
+	}
+
+	avltree_rotate_left(tree, left);
+	avltree_rotate_right(tree, node);
+}
+
+void avltree_rotate_right_left(AVL_Tree* tree, AVL_Node* node) {
+	assert(tree != NULL);
+	assert(node != NULL);
+
+	AVL_Node* right = node->right;
+	if (right == NULL) {
+		return;
+	}
+
+	avltree_rotate_right(tree, right);
+	avltree_rotate_left(tree, node);
+}
+
+void avltree_rebalance(AVL_Tree* tree, AVL_Node* node) {
+	assert(tree != NULL);
+	assert(node != NULL);
+
+	if (abs(node->balance_factor) < 1) {
+		// No rebalance needed
+		return;
+	}
+
+	AVL_Tree_Rebalance_Action rebalance_action;
+
+	if (node->balance_factor < 0) {
+		assert(node->balance_factor == -2);
+		assert(node->left != NULL);
+
+		if (node->left->balance_factor < 0) {
+			rebalance_action = AVL_TREE_REBALANCE_RIGHT;
+		} else {
+			rebalance_action = AVL_TREE_REBALANCE_LEFT_RIGHT;
+		}
+	} else {
+		assert(node->balance_factor == 2);
+		assert(node->right != NULL);
+
+		if (node->right->balance_factor > 0) {
+			rebalance_action = AVL_TREE_REBALANCE_LEFT;
+		} else {
+			rebalance_action = AVL_TREE_REBALANCE_RIGHT_LEFT;
+		}
+	}
+
+	switch (rebalance_action) {
+	case AVL_TREE_REBALANCE_LEFT:
+		avltree_rotate_left(tree, node);
+		break;
+	case AVL_TREE_REBALANCE_RIGHT:
+		avltree_rotate_right(tree, node);
+		break;
+	case AVL_TREE_REBALANCE_LEFT_RIGHT:
+		avltree_rotate_left_right(tree, node);
+		break;
+	case AVL_TREE_REBALANCE_RIGHT_LEFT:
+		avltree_rotate_right_left(tree, node);
+		break;
+	}
+}
+
+void avltree_propagate_balance_change(AVL_Tree* tree, AVL_Node* node, AVL_Tree_Balance_Change change) {
+	int balance_delta;
+	switch (change) {
+	case AVL_TREE_BALANCE_NEW_NODE:
+		balance_delta = 1;
+	case AVL_TREE_BALANCE_DELETED_NODE:
+		balance_delta = -1;
+	}
+
+	AVL_Node* current_node = node;
+	while(current_node->parent != NULL) {
+		AVL_Node* prev_node = current_node;
+		current_node = current_node->parent;
+
+		if (current_node->left == prev_node) {
+			current_node->balance_factor += balance_delta;
+		} else {
+			current_node->balance_factor -= balance_delta;
+		}
+
+		if (abs(current_node->balance_factor) > 1) {
+			avltree_rebalance(tree, current_node);
+			break;
+		}
+	}
+	
+}
+
+void avltree_insert(AVL_Tree* tree, int key, const char* value) {
+	assert(tree != NULL);
+
+	AVL_Node* prev_node = NULL;
+	AVL_Node** insertion_point = &tree->root;
+
+	// Walk down the tree to find an insertion location
+	while (*insertion_point != NULL) {
+		prev_node = *insertion_point;
+
+		if ((*insertion_point)->key > key) {
+			insertion_point = &((*insertion_point)->left);
+		} else {
+			insertion_point = &((*insertion_point)->right);
+		}
+	}
+
+	*insertion_point = avlnode_new(prev_node, key, value);
+	if (prev_node == NULL) {
+		tree->root = *insertion_point;
+	}
+
+	avltree_propagate_balance_change(tree, *insertion_point, AVL_TREE_BALANCE_NEW_NODE);
+}
+
+AVL_Node* avltree_find_node(AVL_Tree tree, int key) {
+	AVL_Node* current_node = tree.root;
+	
+	while (current_node != NULL) {
+		if (key == current_node->key) {
+			return current_node;
+		} else if (key > current_node->key) {
+			current_node = current_node->right;
+		} else {
+			current_node = current_node->left;
+		}
+	}
+
+	return NULL;
+}
+
+void avltree_remove_leaf(AVL_Tree* tree, AVL_Node* leaf) {
+	assert(tree != NULL);
+	assert(leaf != NULL);
+
+	avltree_propagate_balance_change(tree, leaf, AVL_TREE_BALANCE_DELETED_NODE);
+
+	if (leaf->parent == NULL) {
+		tree->root = NULL;
+		return;
+	} else {
+		if (leaf->parent->left == leaf) {
+			leaf->parent->left = NULL;
+		} else {
+			leaf->parent->right = NULL;
+		}
+	}
+
+}
+
+void avltree_remove(AVL_Tree* tree, int key) {
+	AVL_Node* removal_node = avltree_find_node(*tree, key);
+	if (removal_node == NULL) {
+		return;
+	}
+
+	if (removal_node->left == NULL && removal_node->right == NULL) {
+		avltree_remove_leaf(tree, removal_node);
+	} else if (removal_node->left == NULL) {
+		if (removal_node->parent == NULL) {
+			tree->root = removal_node->right;
+			return;
+		} else {
+			if (removal_node->parent->left == removal_node) {
+				removal_node->parent->left = removal_node->right;
+			} else {
+				removal_node->parent->right = removal_node->right;
+			}
+			avltree_propagate_balance_change(tree, removal_node, AVL_TREE_BALANCE_DELETED_NODE);
+		}
+	} else if (removal_node->right == NULL) {
+		if (removal_node->parent == NULL) {
+			tree->root = removal_node->left;
+			return;
+		} else {
+			if (removal_node->parent->right == removal_node) {
+				removal_node->parent->right = removal_node->left;
+			} else {
+				removal_node->parent->left = removal_node->left;
+			}
+			avltree_propagate_balance_change(tree, removal_node, AVL_TREE_BALANCE_DELETED_NODE);
+		}
+	} else {
+		AVL_Node* successor = avlnode_successor(removal_node);
+		assert(successor != NULL);
+
+		avltree_remove_leaf(tree, successor);
+
+		if (removal_node->parent == NULL) {
+			tree->root = successor;
+			successor->parent = NULL;
+		} else {
+			if (removal_node->parent->left == removal_node) {
+				removal_node->parent->left = successor;
+			} else {
+				removal_node->parent->right = successor;
+			}
+		}
+
+		successor->left = removal_node->left;
+		if (successor->left != NULL) {
+			successor->left->parent = successor;
+		}
+
+		successor->right = removal_node->right;
+		if (successor->right != NULL) {
+			successor->right->parent = successor;
+		}
+	}
+
+	avlnode_free(removal_node);
+	return;
+}
+
+const char* avltree_find(AVL_Tree tree, int key) {
+	AVL_Node* search_result = avltree_find_node(tree, key);
+	if (search_result == NULL) {
+		return NULL;
+	} else {
+		return search_result->value;
+	}
+}
+
+void avltree_show_helper(AVL_Node* node, int current_height, bool* is_frist_print) {
+	if (!*is_frist_print) {
+		printf(" ");
+	} else {
+		*is_frist_print = false;
+	}
+
+	if (node == NULL) {
+		printf("NULL");
+		return;
+	}
+
+	int node_height = avlnode_height(node);
+
+	printf("%d:%s:%d", node->key, node->value, node_height);
+	avltree_show_helper(node->left, current_height + 1, is_frist_print);
+	avltree_show_helper(node->right, current_height + 1, is_frist_print);
+}
+
+void avltree_show(AVL_Tree tree) {
+	bool is_first_print = true;
+	avltree_show_helper(tree.root, 1, &is_first_print);
+	printf("\n");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+int main(void) {
+	AVL_Tree tree;
+	avltree_create(&tree);
+
+	char command[255];
+
+	for (;;) {
+		scanf("%s", command); // UHHH-Scaryyy
+
+		if (strcmp(command, "insert") == 0 || strcmp(command, "i") == 0) {
+			int key;
+			char value[1024];
+			scanf("%d %s", &key, value);
+
+			avltree_insert(&tree, key, value);
+		} else if (strcmp(command, "remove") == 0 || strcmp(command, "r") == 0) {
+			int key;
+			scanf("%d", &key);
+
+			avltree_remove(&tree, key);
+		} else if (strcmp(command, "find") == 0 || strcmp(command, "f") == 0) {
+			int key;
+			scanf("%d", &key);
+
+			const char* value = avltree_find(tree, key);
+			printf("%s", value);
+		} else if (strcmp(command, "show") == 0 || strcmp(command, "s") == 0) {
+			avltree_show(tree);
+		} else if (strcmp(command, "exit") == 0 || strcmp(command, "q") == 0) {
+			break;
+		}
+	}
+	
+	avltree_destroy(tree);
+}
+
